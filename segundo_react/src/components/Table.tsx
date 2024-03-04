bb695107bdd72e00d3094a03f2b94e92ad011803
guiemhomar <ghomar@nigul.cide.es>
2024-03-04 16:34:21 +0100

    asdasd

---
+++ segundo_react/src/components/Table.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { filas } from "./lista"
+
+function Table(){
+    return <></>
+}
+export default Table;
---
+++ segundo_react/src/components/lista.ts
@@ -0,0 +1,50 @@
+
+export const filas = [
+    {
+        producto: "manzana",
+        cantidad: 2,
+        precio: 3.4,
+        tienda: "Mercadona",
+        notas: "Comprar 3",
+        comprado : true,
+        imagen: "...",
+    },
+    {
+        producto: "manzana",
+        cantidad: 2,
+        precio: 3.4,
+        tienda: "Mercadona",
+        notas: "Comprar 3",
+        comprado : true,
+        imagen: "...",
+    },
+    {
+        producto: "manzana",
+        cantidad: 2,
+        precio: 3.4,
+        tienda: "Mercadona",
+        notas: "Comprar 3",
+        comprado : true,
+        imagen: "...",
+    },
+    {
+        producto: "manzana",
+        cantidad: 2,
+        precio: 3.4,
+        tienda: "Mercadona",
+        notas: "Comprar 3",
+        comprado : true,
+        imagen: "...",
+    },
+    {
+        producto: "manzana",
+        cantidad: 2,
+        precio: 3.4,
+        tienda: "Mercadona",
+        notas: "Comprar 3",
+        comprado : true,
+        imagen: "...",
+    },
+];
+
+
