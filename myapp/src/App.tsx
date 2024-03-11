798d30b8e4e61814d0ba2d9906c5491f41679fee
guiemhomar <ghomar@nigul.cide.es>
2024-03-11 20:15:47 +0100

    asdsadsads

--- myapp/src/App.tsx
+++ myapp/src/App.tsx
@@ -1,12 +1,12 @@
 import React from "react";
+import TodoList from "./components/TodoList";
 
-import Tabla from "./Tabla";
+
 
 function App(){
   return (
     <div className="App">
-      <h1 className="App-header">Lista de Compras</h1>
-      <Tabla />
+      <TodoList/>
     </div>
   );
 }
--- myapp/src/components/TodoList.tsx
+++ myapp/src/components/TodoList.tsx
@@ -1,49 +1,49 @@
-// import React, { useState, ChangeEvent, FormEvent } from "react";
-// import TodoItem from "./Todoitem";
+import React, { useState, ChangeEvent, FormEvent } from "react";
+import TodoItem from "./Todoitem";
 
-// function TodoList(){
-//     const [tasks, setTasks] = React.useState(["pon mas tareas como esta!"]);
-//     const [newTask, setNewTask] = useState("");
+function TodoList(){
+    const [tasks, setTasks] = React.useState(["pon mas tareas como esta!"]);
+    const [newTask, setNewTask] = useState("");
 
-//     const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
-//         setNewTask(event.target.value);
-//     };
+    const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setNewTask(event.target.value);
+    };
 
-//     const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
-//         event.preventDefault();
-//         if (newTask.trim() !== ""){
-//             setTasks([...tasks, newTask]);
-//             setNewTask("");
-//         }
-//     };
+    const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        if (newTask.trim() !== ""){
+            setTasks([...tasks, newTask]);
+            setNewTask("");
+        }
+    };
 
-//     const handleDeleteTask = (index: number) => {
-//         const updatedTasks = tasks.filter((_, taskIndex) => taskIndex !== index);
-//         setTasks(updatedTasks);
-//     };
+    const handleDeleteTask = (index: number) => {
+        const updatedTasks = tasks.filter((_, taskIndex) => taskIndex !== index);
+        setTasks(updatedTasks);
+    };
 
-//     return (
-//         <div>
-//             <h2>La meva Llista de Tasques</h2>
-//             <ul>
-//                 {tasks.map((task, index) => (
-//                     <div key={index}>
-//                         <TodoItem  content={task} />
-//                         <button onClick={() => handleDeleteTask(index)}>x</button>
-//                     </div>
-//                 ))}
-//             </ul>
-//             <form onSubmit={handleSubmit}>
-//                 <input 
-//                     type="text"
-//                     value={newTask}
-//                     onChange={handleInputChange}
-//                     placeholder="Afegeix una noca tasca"
-//                 />
-//                 <button type="submit">Enviar</button>
-//             </form>
-//         </div>
-//     );
-// }
+    return (
+        <div>
+            <h2>La meva Llista de Tasques</h2>
+            <ul>
+                {tasks.map((task, index) => (
+                    <div key={index}>
+                        <TodoItem  content={task} />
+                        <button onClick={() => handleDeleteTask(index)}>x</button>
+                    </div>
+                ))}
+            </ul>
+            <form onSubmit={handleSubmit}>
+                <input 
+                    type="text"
+                    value={newTask}
+                    onChange={handleInputChange}
+                    placeholder="Afegeix una noca tasca"
+                />
+                <button type="submit">Enviar</button>
+            </form>
+        </div>
+    );
+}
 
-// export default TodoList;
+export default TodoList;
